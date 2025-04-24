c2a23936d38604084e0b3bfdd4048c79f5377356
Whixon <cyril.houppertz@gmail.com>
2025-04-24 17:46:59 +0200

    modified the script

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import time as t
 import keyboard as kb
+import os
 
 
 
@@ -33,6 +34,7 @@ running = True
 
 while running:
     # affichage de l'écran de jeux
+    os.system("cls")
     print("\n\n\n\n=====================================================Snake============================================")
     for i in range(len(snakeNumberPlate)):
         if(i in index):
@@ -45,11 +47,38 @@ while running:
     
     print("======================================================================================================")
 
-    t.sleep(0.5)
+    
+    
+    if(kb.is_pressed("left") and (direction == "up" or direction == "down")):
+        direction = "left"
+    
+    elif(kb.is_pressed("up") and (direction == "left" or direction == "right")):
+        direction = "up"
+    
+    elif(kb.is_pressed("right") and (direction == "up" or direction == "down")):
+            direction = "right"
+    
+    elif(kb.is_pressed("down") and (direction == "left" or direction == "right")):
+        direction = "down"
+    
     
     if(direction == "up"):
+        index[0] -= 23
+    
+    elif(direction == "down"):
+        index[0] += 23
+    
+    elif(direction == "left"):
+        if(snakeNumberPlate[index[0] - 1] == 0):
+            index[0] -= 1
+        elif(snakeNumberPlate[index[0] - 1] == -1):
+            index[0] += 21
+    
+    elif(direction == "right"):
         if(snakeNumberPlate[index[0] + 1] == 0):
             index[0] += 1
         elif(snakeNumberPlate[index[0] + 1] == -1):
-            index[0] += 2
-        
+            index[0] -= 21
+            
+    
+    t.sleep(0.2)
