1dde42754473a426b61c8b2dcfd6ac9328908354
Whixon <cyril.houppertz@gmail.com>
2025-06-12 17:01:31 +0200

    coucou

--- script.py
+++ script.py
@@ -53,33 +53,59 @@ direction = "up"
 
 pomme = False
 
-co_pomme = 0
+co_pomme_row = 5
+co_pomme_col = 7
 
 running = True
 
 while running:
+    snakeNumberPlate = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
     
+    #on définie quel coordonée on affiche ou pas
     for i in range(len(snake_coordinate_bool)):
-        if(i > snake_size):
+        if(i >= snake_size):
             snake_coordinate_bool[i] = False
         else:
             snake_coordinate_bool[i] = True
     
+    #si le snake a manger la pomme on redéfinie une position pour elle
+    if(not pomme):
+        while(co_pomme_col in snake_coordinate_col and co_pomme_row in snake_coordinate_row):
+            co_pomme_row = random.randint(0, row)
+            co_pomme_col = random.randint(0, col)
+    else:
+        snakeNumberPlate[co_pomme_row][co_pomme_col] = 2
+    
+    #ici on définier tout les positions de chaque partie du snake
+    n
+    while n > 0:
+        snake_coordinate_col[i+1] = snake_coordinate_col[i]
+        snake_coordinate_row[i+1] = snake_coordinate_row[i]
+    
+    print(snake_coordinate_row)
+    
+    
+    for i in range(snake_size):
+        snakeNumberPlate[snake_coordinate_row[i]][snake_coordinate_col[i]] = 1
+        
             
 
     # affichage de l'écran de jeux
     os.system("cls")
-    
-    
-    print("\n\n\n\n=====================================================Chargement de la grille Snake============================================")
-    
-    line = ""
-    
-    
-                
-                
-    
-    print("======================================================================================================")
+    print(snake_coordinate_row)
 
     
     
@@ -97,15 +123,36 @@ while running:
     
     
     if(direction == "up"):
-        pass
+        snake_coordinate_row[0] -= 1
     
     elif(direction == "down"):
-        pass
+        snake_coordinate_row[0] += 1
     
     elif(direction == "left"):
-        pass
+        snake_coordinate_col[0] -= 1
     
     elif(direction == "right"):
-        pass
+        snake_coordinate_col[0] += 1
+        
+        
+        
+    print("\n\n\n\n=====================================================Chargement de la grille Snake============================================")
     
-    t.sleep(0.2)
+    line = ""
+    
+    for i in range(row):
+        for j in range(col):
+            if(snakeNumberPlate[i][j] == 1):
+                line += "⬛"
+            elif(snakeNumberPlate[i][j] == 2):
+                line += "🟥"
+            else:
+                line += "⬜"
+        
+        print(line)
+        line = ""
+    
+    
+    print("======================================================================================================")
+    
+    t.sleep(0.5)
